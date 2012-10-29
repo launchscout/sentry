f7985644b4d5c544586a6a63968f2624b473497d
Chris Moore <cdmwebs@gmail.com>
2012-10-28 20:36:26 -0400

    configure SendGrid

--- sentry.conf.py
+++ sentry.conf.py
@@ -22,15 +22,14 @@ SENTRY_WEB_OPTIONS = {
     # 'worker_class': 'gevent',
 }
 
-# Mail server configuration
+if 'MAIL_TO' in os.environ:
+    ADMINS = [('J. Doe', os.environ['MAIL_TO'])]
+    SENTRY_ADMINS = [os.environ['MAIL_TO']]
 
-# For more information check Django's documentation:
-#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
+SERVER_EMAIL = os.environ.get('SENDGRID_USERNAME')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'localhost'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_HOST_USER = ''
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
