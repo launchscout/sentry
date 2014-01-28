4943a762c970fbde2c724f3f3d8b54ab59d070b8
Chris Moore <cdmwebs@gmail.com>
2014-01-28 11:09:44 -0500

    upgrade sentry

--- sentry.conf.py
+++ sentry.conf.py
@@ -1,96 +1,108 @@
-#!/usr/bin/env python
+import logging
+import os
+import sys
+
+from sentry.conf.server import *
+
+ROOT = os.path.dirname(__file__)
+
+sys.path.append(ROOT)
 
 import dj_database_url
-import os
-import os.path
+DATABASES = {'default': dj_database_url.config()}
 
-CONF_ROOT = os.path.dirname(__file__)
 
-DATABASES = {
-    'default': dj_database_url.config(default="postgres://sentry@localhost"),
-}
+# Sentry configuration
+# --------------------
 
-# If you're expecting any kind of real traffic on Sentry, we highly recommend configuring
-# the CACHES and Redis settings
+SENTRY_KEY = os.environ.get('SENTRY_KEY')
 
-# You'll need to install the required dependencies for Memcached:
-#   pip install python-memcached
-#
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#         'LOCATION': ['127.0.0.1:11211'],
-#     }
-# }
-
-# Buffers (combined with queueing) act as an intermediate layer between the database and
-# the storage API. They will greatly improve efficiency on large numbers of the same events
-# being sent to the API in a short amount of time.
-
-# SENTRY_USE_QUEUE = True
-# For more information on queue options, see the documentation for Celery:
-# http://celery.readthedocs.org/en/latest/
-# BROKER_URL = 'redis://localhost:6379'
-
-# You'll need to install the required dependencies for Redis buffers:
-#   pip install redis hiredis nydus
-#
-# SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
-# SENTRY_REDIS_OPTIONS = {
-#     'hosts': {
-#         0: {
-#             'host': '127.0.0.1',
-#             'port': 6379,
-#         }
-#     }
-# }
-
+# Set this to false to require authentication
 SENTRY_PUBLIC = False
-SENTRY_KEY = os.environ.get('SENTRY_KEY', 'secret')
-
-# You should configure the absolute URI to Sentry. It will attempt to guess it if you don't
-# but proxies may interfere with this.
-SENTRY_URL_PREFIX = 'http://sentry.gaslight.co'  # No trailing slash!
 
 SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = os.environ.get('PORT', 9000)
+SENTRY_WEB_PORT = int(os.environ.get('PORT', 9000))
 SENTRY_WEB_OPTIONS = {
-    'workers': 3,  # the number of gunicorn workers
-    'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
+    'workers': 3,
+    'worker_class': 'gevent',
 }
 
-# Mail server configuration
+SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
 
-# For more information check Django's documentation:
-#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# Email configuration
+# -------------------
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-SERVER_EMAIL = os.environ.get('MAIL_FROM')
+# Disable the default admins (for email)
+ADMINS = ()
+
+# Set Sentry's ADMINS to a raw list of email addresses
+SENTRY_ADMINS = os.environ.get('ADMINS', '').split(',')
+
+# The threshold level to restrict emails to.
+SENTRY_MAIL_LEVEL = logging.WARNING
+
+# The prefix to apply to outgoing emails.
+EMAIL_SUBJECT_PREFIX = '[Sentry] '
+
+# The reply-to email address for outgoing mail.
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'root@localhost')
+
+
+# Bcrypt
+# ------
+
+INSTALLED_APPS += ('django_bcrypt',)
+
+# Enables bcrypt password migration on a ``check_password()`` call.
+#
+# The hash is also migrated when ``BCRYPT_ROUNDS`` changes.
+BCRYPT_MIGRATE = True
+
+
+# Social Auth
+# -----------
+
+SOCIAL_AUTH_CREATE_USERS = 'SOCIAL_AUTH_CREATE_USERS' in os.environ
+
+
+# Twitter
+# -------
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+
+
+# Facebook
+# --------
+
+FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+FACEBOOK_API_SECRET = os.environ.get('FACEBOOK_API_SECRET')
+
+
+# Google
+# ------
+
+GOOGLE_OAUTH2_CLIENT_ID = os.environ.get('GOOGLE_OAUTH2_CLIENT_ID')
+GOOGLE_OAUTH2_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET')
+
+
+# GitHub
+# ------
 
 GITHUB_APP_ID = os.environ.get('GITHUB_APP_ID')
 GITHUB_API_SECRET = os.environ.get('GITHUB_API_SECRET')
 GITHUB_EXTENDED_PERMISSIONS = ['repo']
 
-# http://twitter.com/apps/new
-# It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
 
-# http://developers.facebook.com/setup/
-FACEBOOK_APP_ID = ''
-FACEBOOK_API_SECRET = ''
+# Bitbucket
+# ---------
 
-# http://code.google.com/apis/accounts/docs/OAuth2.html#Registering
-GOOGLE_OAUTH2_CLIENT_ID = ''
-GOOGLE_OAUTH2_CLIENT_SECRET = ''
+BITBUCKET_CONSUMER_KEY = os.environ.get('BITBUCKET_CONSUMER_KEY')
+BITBUCKET_CONSUMER_SECRET = os.environ.get('BITBUCKET_CONSUMER_SECRET')
 
-# https://trello.com/1/appKey/generate
-TRELLO_API_KEY = ''
-TRELLO_API_SECRET = ''
